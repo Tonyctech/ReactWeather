f6f117544fc9fbefed3128472d4ab5118c5078f8
BuildTools <tony@tonyctech.com>
2020-11-29 17:25:05 -0600

    Temp Color Changes

--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -6,10 +6,15 @@ import Condition from "./Condition";
 import Icon from "./Icon";
 
 const WeatherCard = (props) => {
-  const red = 100;
+  let highColor = (1 - (props.temp - 12) / 28) * 255;
+  let lowColor = highColor - 150;
   const Card = styled.div`
         margin: 0 auto;
-        background: linear-gradient(to bottom, rgba(${red}, 200, 200), lightblue);
+        background: linear-gradient(
+          to top,
+          rgb(255, ${highColor}, 0),
+          rgb(255, ${lowColor}, 0)
+          );
         width: 200px;
         height: 241px;
         display: flex;
